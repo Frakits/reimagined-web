45680b79d7161b4273982dbe97f76c8a280a9e4d
Frakist <90983918+Frakist@users.noreply.github.com>
2024-01-22 16:53:32 +0200

    test

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const minifyjs = require("terser").minify;
 const minifyhtml = require("html-minifier").minify;
 const cssnano = require("cssnano");
 const postcss = require("postcss");
+const sass = require("sass");
 
 const fs = require("fs");
 let jsCode = fs.readFileSync("src/index.js", "utf-8");
-let cssCode = fs.readFileSync("src/index.css", "utf-8");
+let cssCode = sass.compile("src/index.scss");
 let htmlCode = fs.readFileSync("src/index.html", "utf-8");
 
 fs.cpSync("src/assets", "docs/assets", {recursive: true});
