29c362a7374921aafcbde88b19ba75cb6bf1c899
Frakist <90983918+Frakist@users.noreply.github.com>
2024-01-22 13:23:13 +0200

    test hook

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let jsCode = fs.readFileSync("src/index.js", "utf-8");
 let cssCode = fs.readFileSync("src/index.css", "utf-8");
 let htmlCode = fs.readFileSync("src/index.html", "utf-8");
 
+fs.cpSync("src/assets", "docs/assets", {recursive: true});
 fs.writeFileSync("docs/index.html", minifyhtml(htmlCode, {caseSensitive: true, collapseWhitespace: true, collapseInlineTagWhitespace: true, removeAttributeQuotes: true, removeComments: true, removeEmptyAttributes: true, removeRedundantAttributes: true, minifyCSS: true}))
 minifyjs(jsCode, {toplevel: true}).then(c => fs.writeFileSync("docs/index.js", c.code));
 postcss([cssnano({preset: 'default'})]).process(cssCode, {from: undefined}).then(c => fs.writeFileSync("docs/index.css", c.css));
