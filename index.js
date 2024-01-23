8b096a6093d8e79561c3c8ce3998257e2b4386aa
Frakist <90983918+Frakist@users.noreply.github.com>
2024-01-23 17:59:13 +0200

    testing new css.

--- index.js
+++ index.js
@@ -10,6 +10,5 @@ let cssCode = sass.compile("src/index.scss");
 let htmlCode = fs.readFileSync("src/index.html", "utf-8");
 
 fs.cpSync("src/assets", "docs/assets", {recursive: true});
-fs.writeFileSync("docs/index.html", minifyhtml(htmlCode, {caseSensitive: true, collapseWhitespace: true, collapseInlineTagWhitespace: true, removeAttributeQuotes: true, removeComments: true, removeEmptyAttributes: true, removeRedundantAttributes: true, minifyCSS: true}))
 minifyjs(jsCode, {toplevel: true}).then(c => fs.writeFileSync("docs/index.js", c.code));
-postcss([cssnano({preset: 'default'})]).process(cssCode.css, {from: undefined}).then(c => fs.writeFileSync("docs/index.css", c.css));
+postcss([cssnano({preset: 'default'})]).process(cssCode.css, {from: undefined}).then(c => fs.writeFileSync("docs/index.html", minifyhtml(htmlCode.replace(`<link rel="stylesheet" href="index.css">`, `<style>${c.css}</style>`), {caseSensitive: true, collapseWhitespace: true, collapseInlineTagWhitespace: true, removeAttributeQuotes: true, removeComments: true, removeEmptyAttributes: true, removeRedundantAttributes: true, minifyCSS: true})));
